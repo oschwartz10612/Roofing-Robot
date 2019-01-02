155dbb5a661200559ab0f09958fc087e6a0ac33d
oschwartz10612 <oschwartz10612@gmail.com>
2019-01-01 18:05:14 -0700

    consolidated project to final file stucture state

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,21 @@
 #include <AccelStepper.h>
 #include <MultiStepper.h>
 
+#include <SabertoothSimplified.h>
+
+SabertoothSimplified ST; // We'll name the Sabertooth object ST.
+                         // For how to configure the Sabertooth, see the DIP Switch Wizard for
+                         //   http://www.dimensionengineering.com/datasheets/SabertoothDIPWizard/start.htm
+                         // Be sure to select Simplified Serial Mode for use with this library.
+                         // This sample uses a baud rate of 9600.
+                         //
+                         // Connections to make:
+                         //   Arduino TX->1  ->  Sabertooth S1
+                         //   Arduino GND    ->  Sabertooth 0V
+                         //   Arduino VIN    ->  Sabertooth 5V (OPTIONAL, if you want the Sabertooth to power the Arduino)
+                         //
+                         // If you want to use a pin other than TX->1, see the SoftwareSerial example.
+
 AccelStepper stepper1(AccelStepper::DRIVER, 22, 23);
 AccelStepper stepper2(AccelStepper::DRIVER, 26, 27);
 AccelStepper stepper3(AccelStepper::DRIVER, 30, 31);
@@ -11,6 +26,7 @@ MultiStepper steppers;
 long positions[3];
 
 void setup() {
+  SabertoothTXPinSerial.begin(9600);
   stepper1.setMaxSpeed(35);
   stepper1.setAcceleration(10);
   stepper1.setEnablePin(25);
@@ -30,6 +46,14 @@ void setup() {
   steppers.addStepper(stepper2);
   steppers.addStepper(stepper3);
 
+
+}
+
+void loop() {
+
+}
+
+void eject() {
   positions[0] = 150;
   positions[1] = 370;
   positions[2] = 470;
@@ -49,8 +73,8 @@ void setup() {
   stepper1.disableOutputs();
   stepper2.disableOutputs();
   stepper3.disableOutputs();
-}
 
-void loop() {
-
+  stepper1.setCurrentPosition(0);
+  stepper2.setCurrentPosition(0);
+  stepper3.setCurrentPosition(0);
 }
